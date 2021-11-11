6097d0a8d67293197cb589f5861da90b03d4b3ed
austin <galaky2005@gmail.com>
2021-11-11 16:40:54 -0500

    initial commit; needs to be tested;

---
+++ start.py
@@ -0,0 +1,64 @@
+import sys
+from austin_heller_repo.version_controlled_containerized_python_manager import VersionControlledContainerizedPythonManager, DockerContainerInstanceTimeoutException
+from austin_heller_repo.git_manager import GitManager
+import tempfile
+from typing import List
+import json
+
+git_url = None  # type: str
+script_file_path = None  # type: str
+script_arguments = []  # type: List[str]
+timeout_seconds = None  # type: float
+
+arg_index = 1
+while arg_index < len(sys.argv):
+	if sys.argv[arg_index] == "-g":
+		if git_url is not None:
+			raise Exception(f"Already provided git url. Have \"{git_url}\", found \"{sys.argv[arg_index]}\".")
+		arg_index += 1
+		git_url = sys.argv[arg_index]
+	elif sys.argv[arg_index] == "-s":
+		if script_file_path is not None:
+			raise Exception(f"Already provided script file path. Have \"{script_file_path}\", found \"{sys.argv[arg_index]}\".")
+		arg_index += 1
+		script_file_path = sys.argv[arg_index]
+	elif sys.argv[arg_index] == "-sa":
+		arg_index += 1
+		script_arguments.append(sys.argv[arg_index])
+	elif sys.argv[arg_index] == "-t":
+		if timeout_seconds is not None:
+			raise Exception(f"Already provided timeout seconds. Have \"{timeout_seconds}\", found \"{sys.argv[arg_index]}\".")
+		arg_index += 1
+		timeout_seconds = float(sys.argv[arg_index])
+	else:
+		raise Exception(f"Failed to parse commandline argument: \"{sys.argv[arg_index]}\".")
+
+temp_directory = tempfile.TemporaryDirectory()
+
+git_manager = GitManager(
+	git_directory_path=temp_directory.name
+)
+
+vccpm = VersionControlledContainerizedPythonManager(
+	git_manager=git_manager
+)
+
+vccpi = vccpm.run_python_script(
+	git_repo_clone_url=git_url,
+	script_file_path=script_file_path,
+	script_arguments=script_arguments,
+	timeout_seconds=timeout_seconds
+)
+
+output = {
+	"data": None,
+	"exception": None
+}
+
+try:
+	vccpi.wait()
+	output["data"] = vccpi.get_output()
+except DockerContainerInstanceTimeoutException as ex:
+	output["exception"] = str(ex)
+
+print(json.dumps(output))
